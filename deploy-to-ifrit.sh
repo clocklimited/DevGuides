c254f6a1d2aaf6a54a4ce217699a346375be8ab7
Jack Brewer <jack@jackbrewer.co.uk>
2013-07-10 14:19:50 +0100

    Updating deploy script to stop/start, rather than restart

--- deploy-to-ifrit.sh
+++ deploy-to-ifrit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -A ifrit.clockhosting.com -t 'cd /var/application/Clock/DevGuides; git pull && pliers build && sudo restart node-clock-dev-guides'
+ssh -A ifrit.clockhosting.com -t 'cd /var/application/Clock/DevGuides; git pull && pliers build && sudo stop node-clock-dev-guides && sudo start node-clock-dev-guides'
