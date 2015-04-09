06849379a2d674ab370dabf320ca3537fe553be6
Rob Noake <rob.noake@clock.co.uk>
2015-04-09 16:06:51 +0000

    deployment: use nave for node commands and bump version to avoid npm issue

--- deploy-to-prod.sh
+++ deploy-to-prod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -A clock-shared-node-01.clockhosting.com -t 'cd /var/application/Clock/DevGuides; git pull && pliers build && sudo stop node-clock-dev-guides; sudo start node-clock-dev-guides'
+ssh -A clock-shared-node-01.clockhosting.com -t 'cd /var/application/Clock/DevGuides; git pull && nave use 0.10.33 npm install && nave use 0.10.33 npm i -g pliers && nave use 0.10.33 pliers build && sudo stop node-clock-dev-guides; sudo start node-clock-dev-guides'
